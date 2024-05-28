72544d3e906f6edbdccfce387a9ecd047a476414
uyanga <112911143+uyangabyambaa@users.noreply.github.com>
2024-05-28 22:55:12 +0800

    Update Reports.tsx

--- src/components/containers/Reports.tsx
+++ src/components/containers/Reports.tsx
@@ -20,14 +20,61 @@ const ReportsPage = () => {
                         <tr className='grid grid-cols-12 text-center py-4 px-5'>
                             <td className='col-span-1'>1</td>
                             <td className='col-span-2'>2024-05-05</td>
-                            <td className='col-span-5'>-</td>
+                            <td className='col-span-5'>Уран зохиол</td>
+                            <td className='col-span-2'>1</td>
+                            <td className='col-span-2'>1</td>
+                        </tr>
+                    </tbody>
+                    <tbody className='w-full flex flex-col divide-y divide-[#8A8EBB]'>
+                        <tr className='grid grid-cols-12 text-center py-4 px-5'>
+                            <td className='col-span-1'>2</td>
+                            <td className='col-span-2'>2024-05-05</td>
+                            <td className='col-span-5'>Дүрслэх урлаг</td>
                             <td className='col-span-2'>-</td>
                             <td className='col-span-2'>-</td>
                         </tr>
+                    </tbody>
+                    <tbody className='w-full flex flex-col divide-y divide-[#8A8EBB]'>
                         <tr className='grid grid-cols-12 text-center py-4 px-5'>
-                            <td className='col-span-1'>1</td>
+                            <td className='col-span-1'>3</td>
                             <td className='col-span-2'>2024-05-05</td>
-                            <td className='col-span-5'>-</td>
+                            <td className='col-span-5'>Хөгжмийн урлаг</td>
+                            <td className='col-span-2'>1</td>
+                            <td className='col-span-2'>1</td>
+                        </tr>
+                    </tbody>
+                    <tbody className='w-full flex flex-col divide-y divide-[#8A8EBB]'>
+                        <tr className='grid grid-cols-12 text-center py-4 px-5'>
+                            <td className='col-span-1'>4</td>
+                            <td className='col-span-2'>2024-05-05</td>
+                            <td className='col-span-5'>Тайзны урлаг</td>
+                            <td className='col-span-2'>-</td>
+                            <td className='col-span-2'>-</td>
+                        </tr>
+                    </tbody>
+                    <tbody className='w-full flex flex-col divide-y divide-[#8A8EBB]'>
+                        <tr className='grid grid-cols-12 text-center py-4 px-5'>
+                            <td className='col-span-1'>5</td>
+                            <td className='col-span-2'>2024-05-05</td>
+                            <td className='col-span-5'>Кино урлаг, дуу-дүрсний уран бүтээл</td>
+                            <td className='col-span-2'>-</td>
+                            <td className='col-span-2'>-</td>
+                        </tr>
+                    </tbody>
+                    <tbody className='w-full flex flex-col divide-y divide-[#8A8EBB]'>
+                        <tr className='grid grid-cols-12 text-center py-4 px-5'>
+                            <td className='col-span-1'>6</td>
+                            <td className='col-span-2'>2024-05-05</td>
+                            <td className='col-span-5'>Цахим бүтээл</td>
+                            <td className='col-span-2'>1</td>
+                            <td className='col-span-2'>-</td>
+                        </tr>
+                    </tbody>
+                    <tbody className='w-full flex flex-col divide-y divide-[#8A8EBB]'>
+                        <tr className='grid grid-cols-12 text-center py-4 px-5'>
+                            <td className='col-span-1'>7</td>
+                            <td className='col-span-2'>2024-05-05</td>
+                            <td className='col-span-5'>Дизайн, архитектурын бүтээл</td>
                             <td className='col-span-2'>-</td>
                             <td className='col-span-2'>-</td>
                         </tr>
